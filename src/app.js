ef6505e7f5716cf2924f61b1c5d954332d0bd01d
Prayskey <prayskeyo@gmail.com>
2026-05-28 15:01:37 +0100

    Add extra security measure to protect API from attacks

--- src/app.js
+++ src/app.js
@@ -1,16 +1,34 @@
 import express from 'express';
 import cors from 'cors';
+import helmet from 'helmet';
+import rateLimit from 'express-rate-limit';
 import 'dotenv/config';
 import authRoutes from './routes/auth.routes.js';
 import errorHandler from './middlewares/error.middleware.js';
 
 const app = express();
+const corsOptions = {
+  origin: '*',
+  optionsSuccessStatus: 200,
+};
+// Rate limiting middleware configuration
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minute time window
+  limit: 100, // Limit each IP to 100 requests per window
+  message: 'Too many requests, please try again later.',
+  standardHeaders: 'draft-8', // Includes rate limit info in headers
+});
 
+// Apply globally or to specific routes
+app.use('/api/', limiter); //
+app.use(helmet());
 app.use(express.json());
-app.use(cors({ origin: '*', credentials: true }));
+app.use(cors(corsOptions));
+
+
 
 app.get('/', (req, res) => {
-  res.json({ response: "You've successfully connected to the server!" });
+  res.json("You've successfully connected to the server!");
 });
 
 app.use('/api/auth', authRoutes);
